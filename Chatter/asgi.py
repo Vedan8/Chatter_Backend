930fcff766265efddcbf87beae1e9974c6bfa77f
Vedan8 <vedanjyadav@gmail.com>
2024-09-24 22:04:25 +0530

    fix

--- Chatter/asgi.py
+++ Chatter/asgi.py
@@ -5,7 +5,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from chat.routing import websocket_urlpatterns
 
-print(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Chatter.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
--- chat/models.py
+++ chat/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-# from django.conf import settings
-from core.models import User
+from django.conf import settings
 
 class Chat(models.Model):
     participants = models.ManyToManyField(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name='chats'
     )
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,7 +23,7 @@ class Message(models.Model):
         on_delete=models.CASCADE
     )
     sender = models.ForeignKey(
-        User,
+        settings.AUTH_USER_MODEL,
         related_name='sent_messages',
         on_delete=models.CASCADE
     )
