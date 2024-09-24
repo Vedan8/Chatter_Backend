636a3d7a97a318f0bc7e00adb99e3c84b5c391a6
Vedan8 <131740640+Vedan8@users.noreply.github.com>
2024-09-24 20:03:38 +0530

    Update settings.py

--- Chatter/settings.py
+++ Chatter/settings.py
@@ -162,7 +162,4 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
 
-settings.configure(
-    AUTH_USER_MODEL = 'core.User',
-)
 
