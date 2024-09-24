6fcb06a37508f964957ae38c5bd680c5fd90ff48
Vedan8 <vedanjyadav@gmail.com>
2024-09-24 22:55:59 +0530

    chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,14 +2,15 @@ import json
 from urllib.parse import parse_qs
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from django.contrib.auth import get_user_model
 import jwt
 from Chatter.settings import SECRET_KEY
 
-User = get_user_model()
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     from .models import Chat, Message
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
     async def connect(self):
         self.user = None
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
